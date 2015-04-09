1ed916efa238145952476a41f1f5312001a70f9e
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-04-09 08:07:10 +0200

    Update: testuj.bash

--- testuj.bash
+++ testuj.bash
@@ -5,12 +5,21 @@
 # then
 #    exit 1
 
-if [ `facter virtual` != "virtualbox"  ];
+if [ `whoami` == "travis" ];
 then
+    BASEDIR=`pwd`
+elif [ `facter virtual` == "virtualbox" ];
+then
+    BASEDIR=/vagrant
+else
     echo The command should be executed within the guest OS!
     exit 1
 fi
 
 rm -rf /app/symfony2app/app/cache/*
+
+mysql -u root < "${BASEDIR}/00-extra/db/create-empty-database.sql"
+php "${BASEDIR}/app/console" doctrine:schema:update --force
+php app/console fos:user:create admin admin@example.net loremipsum --super-admin
 
 /app/symfony2app/vendor/behat/behat/bin/behat $@
